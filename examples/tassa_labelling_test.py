1c766e42245a206dbc27f9d4f2f47cf47eda3973
Jansen Wong <51034285+jansenw95@users.noreply.github.com>
2023-02-02 22:53:15 -0500

    added writing to json

--- examples/tassa_labelling_test.py
+++ examples/tassa_labelling_test.py
@@ -1,3 +1,4 @@
+import json
 import uuid
 
 import numpy as np
@@ -9,15 +10,18 @@ from tassa.schemas import Page, Header1, Paragraph, ImageCls, Text, InputBox, Sl
 
 doc = Tassa("ws://localhost:8012")
 
+# make json file that can be appended to
+
+
 
 # this is blocking because it autostarts.
 @doc.bind(start=True)
 def show_heatmap():
     page = Page(
         Scene(
-            Ply(url="https://escher.ge.ngrok.io/files/william/debug/jansen/label.ply", rotation=[-.5 * np.pi, 0, 0],
+            Ply(url="https://escher.ge.ngrok.io/files/william/debug/jansen/label.ply", rotation=[-.5 * np.pi, 0, 0], position=[-.5, 0, 0],
                 movable=True, key="orange"),
-            Gripper(movable=True, handleOffset=[0, .2, 0], pinchWidth=1, skeleton=False, key="gripper"),
+            Gripper(movable=True, handleOffset=[0, .2, 0], pinchWidth=1, skeleton=False, key="gripper", axes=True),
             style={"width": "100vw", "height": "900px"}
         )
     )
@@ -27,6 +31,12 @@ def show_heatmap():
             event = yield Update(ImageCls(data=event.value, key="alan_img"))
         else:
             print(vars(event))
+            # append the results to a json file
+            with open("output.json", "a") as f:
+                # json.dumps (vars(event))
+                f.write(json.dumps(vars(event)))
+                f.write("\n")
+                f.flush()
             res = Paragraph(str(vars(event)), key=str(uuid.uuid4())[-10:])
-            print(res.serialize())
+            # print(res.serialize())
             event = yield Update(res)
