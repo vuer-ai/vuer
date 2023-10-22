d149795e048c26caf335624d4d42f370b65029c4
Ge Yang <yangge1987@gmail.com>
2023-10-22 15:52:53 -0400

    update example

--- examples/vuer/02_pointcloud.py
+++ examples/vuer/02_pointcloud.py
@@ -1,19 +1,14 @@
 from pathlib import Path
 
-import chardet
 import numpy as np
-# import numpy as np
-import trimesh
 import open3d as o3d
 
 from vuer import Vuer
 from vuer.events import Set
-from vuer.schemas import Obj, DefaultScene, Ply, PointCloud
+from vuer.schemas import DefaultScene, Ply, PointCloud
 
 assets_folder = Path(__file__).parent / "../../assets"
 test_file = "static_3d/porsche.ply"
-# test_file = "pixelnerf.ply"
-# test_file = "porsche.ply"
 
 # trimesh has issue loading large pointclouds.
 pcd = o3d.io.read_point_cloud(str(assets_folder / test_file))
@@ -25,8 +20,9 @@ app = Vuer(static_root=assets_folder)
 async def main(ws):
     app @ Set(
         DefaultScene(
-            Ply(src="http://localhost:8012/static/" + test_file, size=0.008, position=[0, 0, 1], rotation=[-0.5 * np.pi, 0, -0.5 * np.pi]),
-            # PointCloud(key="pointcloud", vertices=np.array(pcd.points), colors=np.array(pcd.colors), position=[0, 0, 0], size=0.008),
+            Ply(src="http://localhost:8012/static/" + test_file, size=0.008, position=[0, 0, 5]),
+            # rotation=[-0.5 * np.pi, 0, -0.5 * np.pi]),
+            PointCloud(key="pointcloud", vertices=np.array(pcd.points), colors=np.array(pcd.colors), position=[-2, 0, 0], size=0.008),
         ),
 
     )
