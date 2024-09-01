0518c57ee0e289184fa6498a1bba611faf2909a6
Ge Yang <ge.ike.yang@gmail.com>
2024-09-01 01:17:49 -0400

    update formatting

--- docs/examples/02_pointcloud.md
+++ docs/examples/02_pointcloud.md
@@ -1,28 +1,19 @@
 # Showing Point Clouds Programmatically and Fast💨
 
-This example shows you two ways to load a pointcloud.
+This example shows you two ways to load a point cloud. In the first example, you serve the point cloud as an `ply` file, and have the webclient read directly from your file system. This approach, however can be a bit slow, and won't work with point cloud data that are updated at real time. In the second example, you load the point cloud into python and then send the parsed vertices and the color information via the `PointCloud` component.
 
-In the first example, you serve the pointcloud as an `ply` file, and have the webclient read direclty from your file system.
-
-This approach, however can be a bit slow, and won't work with pointcloud data that are updated at real time. In the second example, you load
-the pointcloud into python and then send the parsed vertices and the color information via the `PointCloud` component.
-
-We apply a few tricks to make the pointcloud transmit faster. See the [Why is it so much faster?](#why-is-it-so-much-faster) section for
-more details.
+We apply a few tricks to make the point cloud transmit faster. See the [Why is it so much faster?](#why-is-it-so-much-faster) section for more details.
 
 You should expect to see a scene that looks like the following:
 ![pointcloud](figures/pointcloud.png)
 
-The second pointcloud should load significantly
-faster than the first one, due to its smaller size.
-We use half-precision for the vertices, and Uint8
-for the colors. This cuts the overall size by half.
+The second point cloud should load **significantly** faster than the first one, due to its smaller size. We use half-precision for the vertices, and Uint8 for the colors. This cuts the overall size by half.
 
 ## Why is it so much faster?
 
 ```{admonition} Why is it so much faster?
 :class: tip
-We accelerate pointcloud rendeirng by using a custom, half-precision format for the vertices, and Uint8 for the colors. This cuts the overall size by half.
+We accelerate point cloud view by using a custom, half-precision format for the vertices, and Uint8 for the colors. This cuts the overall size by half.
 
 Alternative is to use compression algorithms designed specifically for geometric data (such as [Draco](https://google.github.io/draco/), or just simple LZ4 (or LZW). For depth images, LZW tend to work well due to near-by pixels being highly correlated.
 ```
--- docs/examples/02_pointcloud.py
+++ docs/examples/02_pointcloud.py
@@ -1,33 +1,27 @@
-from cmx import doc
 import os
 from contextlib import nullcontext
+
+from cmx import doc
 
 MAKE_DOCS = os.getenv("MAKE_DOCS", None)
 
 doc @ """
 # Showing Point Clouds Programmatically and Fast💨
 
-This example shows you two ways to load a pointcloud.
+This example shows you two ways to load a point cloud. In the first example, you serve the point cloud as an `ply` file, and have the webclient read directly from your file system. This approach, however can be a bit slow, and won't work with point cloud data that are updated at real time. In the second example, you load the point cloud into python and then send the parsed vertices and the color information via the `PointCloud` component.
 
-In the first example, you serve the pointcloud as an `ply` file, and have the webclient read direclty from your file system.
-
-This approach, however can be a bit slow, and won't work with pointcloud data that are updated at real time. In the second example, you load the pointcloud into python and then send the parsed vertices and the color information via the `PointCloud` component. 
-
-We apply a few tricks to make the pointcloud transmit faster. See the [Why is it so much faster?](#why-is-it-so-much-faster) section for more details.
+We apply a few tricks to make the point cloud transmit faster. See the [Why is it so much faster?](#why-is-it-so-much-faster) section for more details.
 
 You should expect to see a scene that looks like the following:
 ![pointcloud](figures/pointcloud.png)
 
-The second pointcloud should load significantly 
-faster than the first one, due to its smaller size.
-We use half-precision for the vertices, and Uint8
-for the colors. This cuts the overall size by half.
+The second point cloud should load **significantly** faster than the first one, due to its smaller size. We use half-precision for the vertices, and Uint8 for the colors. This cuts the overall size by half.
 
 ## Why is it so much faster?
 
 ```{admonition} Why is it so much faster?
 :class: tip
-We accelerate pointcloud rendeirng by using a custom, half-precision format for the vertices, and Uint8 for the colors. This cuts the overall size by half.
+We accelerate point cloud view by using a custom, half-precision format for the vertices, and Uint8 for the colors. This cuts the overall size by half.
 
 Alternative is to use compression algorithms designed specifically for geometric data (such as [Draco](https://google.github.io/draco/), or just simple LZ4 (or LZW). For depth images, LZW tend to work well due to near-by pixels being highly correlated.
 ```
@@ -46,10 +40,11 @@ with doc, doc.skip if MAKE_DOCS else nullcontext():
     assets_folder = Path(__file__).parent / "../../../assets"
     test_file = "static_3d/porsche.ply"
 
-    # trimesh has issue loading large pointclouds.
+    # trimesh has issue loading large point clouds.
     pcd = o3d.io.read_point_cloud(str(assets_folder / test_file))
 
     app = Vuer(static_root=assets_folder)
+
 
     @app.spawn(start=True)
     async def main(proxy):
