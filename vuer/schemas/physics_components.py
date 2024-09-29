cca9e222582c32e86ae0ca3088219f5a0dece13b
Ge Yang <ge.ike.yang@gmail.com>
2024-09-29 11:54:35 -0400

    add working mocap, restart physics on Move.

--- vuer/schemas/physics_components.py
+++ vuer/schemas/physics_components.py
@@ -14,15 +14,18 @@ class MuJoCo(SceneElement):
     workDir: str = None
     assets: List[str] = []
     speed: float = 1.0
-    pause: bool = True
+    pause: bool = False
     fps: int = 60
     timeout: float = 0.0
+    threshold: float = 0.001
     keyFrames: List[str] = []
 
     # Allow adding additional children for the user to control.
     # actually this can be done outside.
     selfProvide = True
     useLights = True
+    useMocap = True
+    gizmoScale = 0.3
 
     # def __init__(self, src: str, assets: List[str] = [], workDir: str = None, speed: float = 1.0, pause: bool = True, fps: int = 60, timeout: float = 0.0, keyFrames: List[str] = [], **kwargs):
     #     super().__init__(**kwargs)
