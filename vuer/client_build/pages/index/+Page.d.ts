c64d293f960ba3e914deaa33c293a2eff2ce3316
Ge Yang <ge.ike.yang@gmail.com>
2025-04-26 23:03:52 -0400

    fix the simulation pause handling. update mocap pose uses stale mocap_pos TypedArray buffer.

--- vuer/client_build/pages/index/+Page.d.ts
+++ vuer/client_build/pages/index/+Page.d.ts
@@ -1,9 +1,9 @@
 import { default as React } from 'react';
 import { default as ReactDOM } from 'react-dom';
+import * as VUER from '@vuer-ai/vuer';
 import * as THREE from 'three';
 import * as JSX from 'react/jsx-runtime';
 import * as FIBER from '@react-three/fiber';
-import * as VUER from '@vuer-ai/vuer';
 import * as Drei from '@react-three/drei';
 import * as XR from '@react-three/xr';
 declare global {
