c9a3fd1b548fe47e86fb82223b44ad8912713844
jason.bao <jason.bao@57blocks.com>
2025-11-11 14:54:55 +0800

    feat: fix the issue of base64 encoded scene parsing failure & update client_build

--- src/vuer/client_build/assets/chunks/chunk-BrI55jUa.js
+++
@@ -1,1 +0,0 @@
-import{r as t,U as s}from"./chunk-Bxh3mmdk.js";var o=()=>{const e=t.useContext(s);if(e===void 0)throw new Error("useSession must be used within a SessionContext");return e};export{o as u};
--- src/vuer/client_build/env-config.js
+++
@@ -1,4 +0,0 @@
-// This file is generated at build time and exposed to the browser
-// It provides environment variables at runtime
-window.ENV = {
-};
